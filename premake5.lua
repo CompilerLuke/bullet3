ae6b0ab0c8ca93eeaf51401281542b49a8fafde9
CompilerLuke <lgoetz@islux.lu>
2020-11-11 19:06:05 +0100

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,7 @@
+group "Dependencies/Bullet3"
+    require "src/BulletInverseDynamics/premake4"
+    require "src/BulletSoftBody/premake4"
+    require "src/BulletDynamics/premake4"
+    require "src/BulletCollision/premake4"
+    require "src/LinearMath/premake4"
+
